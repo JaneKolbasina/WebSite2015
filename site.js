04d08f2422587697941e7e808043bc102ed2694b
sergei.kolbasin <sergei.kolbasin@gmail.com>
2015-05-13 23:00:26 +0300

    publish gallery

--- site.js
+++ site.js
@@ -1,7 +1,22 @@
-(function($){
+'use strict';
+
+(function(window, $){
     $(function(){
 
-        $('.button-collapse').sideNav();
+        if(Materialize) {
+            window.onerror = function OnError(errorMsg, url, lineNumber, column, errorObj) {
+                var message = 'Error: ' + errorMsg + ' Script: ' + url
+                    + ' Line: ' + lineNumber + ' Column: ' + column
+                    + ' StackTrace: ' +  errorObj;
+                Materialize.toast(message, 4000);
+                return false;
+            };
 
+            $('.button-collapse').sideNav();
+        }
+
+        if($.fn.fancybox) {
+            $(".fancybox").fancybox();
+        }
     });
-})(jQuery);
+})(window, jQuery);
